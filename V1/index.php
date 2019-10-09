a965c21083c1fdf0255d99b36b7bc61a435e3a78
Alexis_Daumur <daumur.alexis@gmail.com>
2019-10-09 16:22:30 +0200

    Yesterday I just created my repository and started thinking about my database and schema. Today I will create a kanban to cut the project into several tasks to do. I will also try to make my base and my schema.

--- V1/index.php
+++ V1/index.php
@@ -1,9 +1,11 @@
 <!-- PAGE PRINCIPALE -->
 
 
+<?php setcookie ('name', 'Alexis', time()+365*24*3600); ?>
+
 <?php require_once 'header.php'; ?>
 <br>
 <br>
 <br>
-<h1>Bienvenue sur le panel administrateur pour gérer votre magasin Chaustore.</h1>
+<h1>Bienvenue <?php echo $_COOKIE['name'] ?> sur le panel administrateur pour gérer votre magasin Chaustore.</h1>
 <h2>Cliquez sur la bulle que vous voulez modifier.</h2>
